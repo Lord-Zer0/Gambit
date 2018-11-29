66520b7b541cea42c0f84a454b6703d36453fb63
Connor McCauley <cdmc13@outlook.com>
2018-11-29 16:11:17 -0500

    script to rotate the board view based on turn

---
+++ Assets/Scripts/RotateView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateView : MonoBehaviour {
+	bool isStandard = true;
+
+	void Update() {
+		RotateCamera();
+	}
+
+	private void RotateCamera() {
+		if (BoardManager.Instance.isWhiteTurn != isStandard) {
+			transform.RotateAround(transform.position, Vector3.up, 180);
+
+			isStandard = !isStandard;
+		}
+		
+	}
+}
