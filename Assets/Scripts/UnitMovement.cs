e75d035fbdd23c3c3abd38dfd16b29a13945ea6f
Connor McCauley <cdmc13@outlook.com>
2018-11-28 19:21:32 -0500

    Create script for moving units

---
+++ Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitMovement : MonoBehaviour {
+
+	public string unitType; // Identify which moveSet is open to the unit
+	public bool canMove; // Determine if there are spots on the movement map where
+	public Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		unitType = this.tag.ToString();
+		print(unitType);
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
