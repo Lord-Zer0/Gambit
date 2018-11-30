409a16aefa22d604e2def07dd9d139b2903c06b0
Connor McCauley <cdmc13@outlook.com>
2018-11-30 00:08:12 -0500

    created simple turn indicator

---
+++ Assets/Scripts/TurnIndicator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnIndicator : MonoBehaviour {
+	public GameObject whiteTurnIcon;
+	public GameObject blackTurnIcon;
+
+	void Start() {
+
+	}
+
+	void Update() {
+		if (BoardManager.Instance.isWhiteTurn) {
+			whiteTurnIcon.SetActive(true);
+			blackTurnIcon.SetActive(false);
+		} else {
+			whiteTurnIcon.SetActive(false);
+			blackTurnIcon.SetActive(true);
+		}
+	}
+}
